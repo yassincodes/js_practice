488f7ca58fbaa742daba9c96a03c6e30498a9afd
yassin <meddyassin@hotmail.com>
2022-02-02 23:36:20 +0100

    .

--- your-personality-in-emojis/index.js
+++ your-personality-in-emojis/index.js
@@ -1,3 +1,4 @@
+// building the app in 8 steps ..
 // 1. Loop through the myEmojis array and log each of them out to the console
 // 2. Grab the emojiContainer div and store it in a variable. Log it to the console
 // 3. Render the emojis as <span> tags in the emojisContainer <div>
@@ -6,13 +7,16 @@
 // 5. Push the emoji into the myEmoji's array, and clear the input field
 //    However, if the input value is empty, don't do anything
 // 6. Render the updated myEmojis array in the mini-browser.
-// 7. ..
-// Make the unshift button work just as the push button (but with unshifting instead of pushing)
+// 7. Make the unshift button work just as the push button
+//    (but with unshifting instead of pushing)
+// 8. Make the pop and shift buttons work as well
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
 const emojiContainer = document.getElementById("emoji-container")
 const emojiInput = document.getElementById("emoji-input")
 const pushBtn = document.getElementById("push-btn")
 const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
 
 function renderEmojis() {
     emojiContainer.innerHTML = ""
@@ -39,4 +43,14 @@ unshiftBtn.addEventListener("click", function(){
         emojiInput.value = ""
         renderEmojis()
     }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
 })
