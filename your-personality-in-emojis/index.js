8ad476e93eb394b4ddf3192c90f465091e1f5d7d
yassin <meddyassin@hotmail.com>
2022-02-02 22:52:31 +0100

    .

--- your-personality-in-emojis/index.js
+++ your-personality-in-emojis/index.js
@@ -1,6 +1,8 @@
 // 1. Loop through the myEmojis array and log each of them out to the console
-
+// 2. Grab the emojiContainer div and store it in a variable. Log it to the console
+// 3. Render the emojis as <span> tags in the emojisContainer <div>
 const myEmojis = ["👨‍💻", "⛷", "🍲"]
+const emojiContainer = document.getElementById("emojiContainer")
 
 for (let i = 0; i < myEmojis.length; i++) {
     console.log(myEmojis[i])
