7b3d67022e148b3b5092697c2438e5b0a68e84d4
yassin <meddyassin@hotmail.com>
2022-02-07 22:57:37 +0100

    🐶

--- dice-game/index.js
+++ dice-game/index.js
@@ -12,28 +12,63 @@ const message = document.getElementById("message")
 const rollBtn = document.getElementById("rollBtn")
 const resetBtn = document.getElementById("resetBtn")
 
+function showDisplayButton() {
+    rollBtn.style.display = "none"
+    resetBtn.style.display = "block"
+}
+
 /* Hook up a click event listener to the Roll Dice Button. */
  rollBtn.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * 6) + 1
 
-    // 1. Update the scores for each player
-    // 2. Display the scores in their scoreboards
     if (player1Turn) {
+        player1Score += randomNumber
+        player1Scoreboard.textContent = player1Score
         player1Dice.textContent = randomNumber
         player1Dice.classList.remove("active")
         player2Dice.classList.add("active")
-        player1Score += randomNumber
-        player1Scoreboard.innerHTML = player1Score
         message.textContent = "Player 2 Turn"
     } else {
+        player2Score += randomNumber
+        player2Scoreboard.textContent = player2Score
         player2Dice.textContent = randomNumber
         player2Dice.classList.remove("active")
         player1Dice.classList.add("active")
-        player2Score += randomNumber
-        player2Scoreboard.innerHTML = player2Score
         message.textContent = "Player 1 Turn"
     }
+    
+    
+    if (player1Score >= 20) {
+        message.textContent = "Player 1 has won! 🥳"
+        showDisplayButton()
+    } else if (player2Score >= 20) {
+        message.textContent = "Player 2 has won! 🎉"
+        showDisplayButton()
+    }
+    
     player1Turn = !player1Turn
 })
  
+// repeat the code :
+// 1. Hook a click event listener up with the Reset Button
+// 2. Create a reset() function that resets the game
+// 3. Invoke the reset() function when the Reset Button is clicked
+
+resetBtn.addEventListener("click", function(){
+    reset()
+})
  
+function reset() {
+    message.textContent = "Player 1 Turn"
+    player1Scoreboard.textContent = 0
+    player2Scoreboard.textContent = 0
+    player1Dice.textContent = '-'
+    player2Dice.textContent = '-'
+    player1Score = 0
+    player2Score = 0
+    player1Turn = true
+    resetBtn.style.display = "none"
+    rollBtn.style.display = "block"
+    player2Dice.classList.remove("active")
+    player1Dice.classList.add("active")
+}
