ec52b87ee0d30d72d241c0f27b2f38895505ba24
yassin <meddyassin@hotmail.com>
2022-02-15 22:43:34 +0100

    .

--- #ScrimaChallengeWeek/day2/index.js
+++ #ScrimaChallengeWeek/day2/index.js
@@ -6,18 +6,31 @@ const greetingDisplayFrom = document.getElementById("greeting-display_from")
 const recipientInput = document.getElementById("recipient-input")
 const greetingSelect = document.getElementById("greeting-select")
 const senderInput = document.getElementById("sender-input")
+const greetingCustomMessageInput = document.querySelector(".greeting-custom-message-input-none")
+const btn = document.getElementById("btn")
 
+btn.addEventListener('click', function(e) {
+    e.preventDefault()
+    greetingCustomMessageInput.classList.remove('greeting-custom-messge-input-none')
+    greetingCustomMessageInput.classList.add('greeting-custom-message-input-show');
+})
 form.addEventListener('input', function() {
     greetingWillGoHere.textContent = ''
     writeGreeting()
 })
-
 function writeGreeting() {
     greetingDisplayMessage.innerHTML = `${greetingSelect.value}`
     if (recipientInput.value != '') {
-        greetingDisplayTo.innerHTML = `to ${recipientInput.value}`
+        greetingDisplayTo.innerHTML = `dear ${recipientInput.value}`
     }
     if (senderInput.value != '') {
-        greetingDisplayFrom.innerHTML = `from ${senderInput.value}`
+        greetingDisplayFrom.innerHTML = `all the love, ${senderInput.value} ❤️`
     }
 }
+
+
+
+
+
+
+
