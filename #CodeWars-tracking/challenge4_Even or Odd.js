a7b704436dcada2c615951ce3d25b1db7527fc70
yassin <meddyassin@hotmail.com>
2022-02-28 23:28:24 +0100

    .

---
+++ #CodeWars-tracking/challenge4_Even or Odd.js
@@ -0,0 +1,9 @@
+function even_or_odd(number) {
+  
+    // you should know what left from the number
+    if (number/2 % 1 === 0 ){
+      return "Even"
+    } else {
+      return "Odd"
+    }
+  }
