c55b7095df543e6f6078c20566ebb1c561148908
yassin <meddyassin@hotmail.com>
2022-03-01 23:23:28 +0100

    changing names

--- #CodeWars-tracking/challenge17_Create-Phone-Number.js
+++
@@ -1,6 +0,0 @@
-function createPhoneNumber(numbers){
-    const numbersString = numbers.join('').toString()
-    return "(" + numbersString.slice(0,3) + ")" 
-               + " " + numbersString.slice(3,6)
-               + "-" + numbersString.slice(6,10) 
-   }
